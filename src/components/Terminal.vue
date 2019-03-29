1c76bca38d567180a7b64b023aa7e964ffbcd7eb
zsamotw <donkemto@interia.pl>
2019-03-29 13:53:54 +0100

    Remove terminal outline.

--- src/components/Terminal.vue
+++ src/components/Terminal.vue
@@ -2,43 +2,45 @@ var Terminal = Vue.component('Terminal', {
 import { request } from 'http';
 <template>
   <div>
-    <i class="fas fa-terminal"></i><input v-model="command" v-on:keyup.enter="addMessage" type="text" name="terminal" autofocus>
+    <i class="fas fa-terminal"></i>
+    <input v-model="command" v-on:keyup.enter="addMessage" type="text" name="terminal" autofocus>
   </div>
 </template>
 
 <script>
-import { process } from '../features/mainProcessor.js'
-import { state } from '../db/state.js'
+import { process } from "../features/mainProcessor.js";
+import { state } from "../db/state.js";
 
 export default {
-  data: function () {
+  data: function() {
     return {
-      command: '',
-      message: '',
+      command: "",
+      message: "",
       gameState: state
-    }
+    };
   },
   methods: {
-    addMessage () {
-      const { state, message } = process(this.command, this.gameState)
-      this.gameState = state
-      this.message = message
-      this.$emit('message', message)
-      this.message = ''
-      this.command = ''
+    addMessage() {
+      const { state, message } = process(this.command, this.gameState);
+      this.gameState = state;
+      this.message = message;
+      this.$emit('message', message);
+      this.message = '';
+      this.command = '';
     }
   }
-}
+};
 </script>
 
-<style  scoped>
+<style scoped>
 input {
   border: none;
   margin-left: 5px;
+  outline: none;
 }
 
-input :hover {
-  border: none;
+input :focus {
+  outline: none;
 }
 </style>
 })
