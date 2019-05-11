667205abd3016bc1605c5c2427c26edc11becb09
zsamotw <donkemot@interia.pl>
2019-05-11 20:15:41 +0200

    Change config files.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,20 @@
-// https://eslint.org/docs/user-guide/configuring
-
 module.exports = {
-    root: true,
-    parserOptions: {
-        parser: 'babel-eslint'
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true
     },
-    env: {
-        browser: true,
+    "extends": "standard",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
     },
-    extends: [
-        // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
-        // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-        'plugin:vue/essential',
-        // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-        'standard'
+    "parserOptions": {
+        "ecmaVersion": 2018
+    },
+    "plugins": [
+        "vue"
     ],
-    // required to lint *.vue files
-    plugins: [
-        'vue'
-    ],
-    // add your custom rules here
-    rules: {
-        // allow async-await
-        'generator-star-spacing': 'off',
-        'semi': ["error", "never"],
-        // allow debugger during development
-        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    "rules": {
     }
-}
+};
