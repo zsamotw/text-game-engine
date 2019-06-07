1b646fa3badd3bcf68b33dee952eeee5ab5ecb22
zsamotw <donkemot@interia.pl>
2019-06-07 23:13:43 +0200

    Update prettier config.

--- prettier.config.js
+++ prettier.config.js
@@ -1,7 +1,12 @@
 // prettier.config.js or .prettierrc.js
 module.exports = {
-  trailingComma: "es6",
+  trailingComma: "es5",
   tabWidth: 2,
   semi: false,
-  singleQuote: true
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: "none",
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  arrowParens: "avoid"
 };
