beeaab24ed283dee94af8d9f671290fba196d822
zsamotw <donkemot@interia.pl>
2019-06-17 22:35:11 +0200

    Refactor generic helper.

--- src/features/helpers/genericHelper.js
+++ src/features/helpers/genericHelper.js
@@ -2,19 +2,10 @@ const R = require('ramda')
 
 const getRandomInt = max => Math.floor(Math.random() * Math.floor(max))
 
-const mapToValues = iterable => {
-  return R.filter(elem => elem !== undefined && elem !== null, iterable)
-}
+const mapToValues = iterable => R.filter(i => !R.isNil(i), iterable)
 
-const changePropertyOfIterable = (iterable, id, propName, newValue) => {
-  return R.map(elem => {
-    if (elem.id === id) {
-      return R.assoc(propName, newValue, elem)
-    } else {
-      return elem
-    }
-  }, iterable)
-}
+const changePropertyOfIterable = (iterable, id, propName, newValue) =>
+  R.map(elem => elem.id === id ? R.assoc(propName, newValue, elem) : elem, iterable)
 
 module.exports = {
   getRandomInt,
