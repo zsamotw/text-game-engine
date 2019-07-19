b5aab008e5a0711c999cda4c6e13046f3849ad97
tomasz <donkemot@interia.pl>
2019-07-19 11:18:19 +0000

    Remove comments from pocket functions.

--- src/features/domain/pocket-functions.ts
+++ src/features/domain/pocket-functions.ts
@@ -7,92 +7,13 @@ import Elem from '../../models/elem'
 
 const maxPocketSize = 2
 
-// const getPocket: (state: State) => Elem[] = R.view(L.pocketLens)
-
 const isPlaceInPocket: (pocket: Elem[]) => boolean = pocket =>
   pocket.length < maxPocketSize
 
 const addElemTo = (elem: Elem, pocket: Elem[]) => R.append(elem, pocket)
 
-// const putElemToPocket: (
-//   elem: Elem,
-//   state: State
-// ) => { state: State; elem: Elem } = (elem, state) => {
-//   const pocket = getPocket(state)
-//   const addElemTo = R.append(elem)
-//   const stateAfterPutElemInPocket = R.set(
-//     L.pocketLens,
-//     addElemTo(pocket),
-//     state
-//   )
-
-//   return { state: stateAfterPutElemInPocket, elem: elem }
-// }
-
-// const removeElemFromStage: (elemAndState: {
-//   elem: Elem
-//   state: State
-// }) => State = elemAndstate => {
-//   const { elem, state } = elemAndstate
-//   const elemsInCurrentStage = SF.getElemsForCurrentStage(state)
-//   const elemsAfterTakeElem = elemsInCurrentStage.filter(
-//     e => e.name !== GH.nameOf(elem)
-//   )
-
-//   const stagesAfterTakeElem = GH.updateIterable(
-//     SF.getStages(state),
-//     SF.getCurrentStageId(state),
-//     'elems',
-//     elemsAfterTakeElem
-//   )
-//   const stateAfterTakeElemFromStage = R.set(
-//     L.stagesLens,
-//     stagesAfterTakeElem,
-//     state
-//   )
-
-//   return stateAfterTakeElemFromStage
-// }
-
-// const putElemToStage: (elem: Elem, state: State) => State = (elem, state) => {
-//   const addElemToElemsInCurrentStage = R.compose(
-//     R.append(elem),
-//     SF.getElemsForCurrentStage
-//   )
-
-//   const stagesAfterPutElem = GH.updateIterable(
-//     SF.getStages(state),
-//     SF.getCurrentStageId(state),
-//     'elems',
-//     addElemToElemsInCurrentStage(state)
-//   )
-
-//   const pocket = getPocket(state)
-//   const takeElemFrom = R.reject(R.propEq('name')(GH.nameOf(elem)))
-
-//   const swapElemFromPocketToStage = (state: State) => {
-//     const stateAfterPutElemInStage = R.set(
-//       L.stagesLens,
-//       stagesAfterPutElem,
-//       state
-//     )
-//     const stateAfterTakeElemFromPocket = R.set(
-//       L.pocketLens,
-//       takeElemFrom(pocket),
-//       stateAfterPutElemInStage
-//     )
-//     return stateAfterTakeElemFromPocket
-//   }
-
-//   return swapElemFromPocketToStage(state)
-// }
-
 export {
-  // getPocket,
   addElemTo,
   isPlaceInPocket,
-  maxPocketSize,
-  // putElemToPocket as addElemToPocket,
-  // removeElemFromStage,
-  // putElemToStage
+  maxPocketSize
 }
