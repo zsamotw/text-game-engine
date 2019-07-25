5f66ea12eea9162155024fa37c2e230c3c083c7f
zsamotw <donkemot@interia.pl>
2019-07-25 08:33:11 +0200

    Update unused imports and gitignore

--- src/features/domain/elems-functions.ts
+++ src/features/domain/elems-functions.ts
@@ -1,6 +1,5 @@
 import * as R from 'ramda'
 import Stage from '../../models/stage'
-import Elem from '../../models/elem'
 import * as L from '../utils/lenses'
 
 const getStageById = R.curry((i: number, stages: Stage[]) =>
