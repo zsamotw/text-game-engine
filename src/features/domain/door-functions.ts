22cc4ff6556b8875d3de6dfb971d4ab13cdcb6d5
zsamotw <donkemot@interia.pl>
2019-09-11 21:56:42 +0200

    Improve method for changing statge by actors

--- src/features/domain/door-functions.ts
+++ src/features/domain/door-functions.ts
@@ -3,19 +3,13 @@ import * as L from '../utils/lenses'
 import * as SF from './stage-functions'
 import Doors from '../../models/doors'
 import Stage from '../../models/stage'
+import { getRandomInt } from './general-usage-functions'
 
 const doorsOf: (stage: Stage) => Doors = R.view(L.doorsLens)
 
 const getWayOut = (doors: Doors) => {
-  if (doors.west !== undefined) {
-    return doors.west
-  } else if (doors.south !== undefined) {
-    return doors.south
-  } else if (doors.east !== undefined) {
-    return doors.east
-  } else if (doors.north !== undefined) {
-    return doors.north
-  }
+  const wayOut = R.compose(getRandomInt, R.length, openedDoors)
+  return wayOut(doors) 
 }
 
 const doorsForCurrentStage: (
@@ -31,19 +25,18 @@ const doorsForStage = R.compose(
   SF.stageFrom
 )
 
-const openDoors = (doors: Doors | undefined) => {
-  const getValues = (doors: Doors) => {
-    return R.reject(R.isNil)(R.values(doors))
-  }
-  R.ifElse(R.isNil, R.always(undefined), getValues)
+const openedDoors = (doors: Doors | undefined) => {
+  const getValues = (doors: Doors) =>  R.reject(R.isNil)(R.values(doors))
+  return  R.ifElse(R.isNil, R.always(undefined), getValues)(doors)
 }
 
 const openedDoorsForStage = R.compose(
-  openDoors,
+  openedDoors,
   doorsForStage
 )
 
 export {
+  openedDoors,
   doorsForCurrentStage,
   doorsForStage,
   openedDoorsForStage,
